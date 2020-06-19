4203b1df5d0d7636e1581d71eebba1b6e1cac664
Jorge Sainz <jorgesainzsueiro@gmail.com>
2020-06-19 18:07:15 +0200

    Station selection Screen
    Added outlets and viewcontroller to station screen
    Fixed some bugs
    Data retrieving and navigation done. (maybe futher refactoring)

---
+++ RyanairMobileTest/RyanairMobileTest/Modules/StationForm/ViewController/StationFormLayout.swift
@@ -0,0 +1,9 @@
+//
+//  StationFormLayout.swift
+//  RyanairMobileTest
+//
+//  Created by Jorge Sainz Sueiro on 19/06/2020.
+//  Copyright © 2020 Jorge Sainz Sueiro. All rights reserved.
+//
+
+import Foundation
---
+++ RyanairMobileTest/RyanairMobileTest/Modules/StationForm/ViewController/StationFormViewController.swift
@@ -0,0 +1,9 @@
+//
+//  StationFormViewController.swift
+//  RyanairMobileTest
+//
+//  Created by Jorge Sainz Sueiro on 19/06/2020.
+//  Copyright © 2020 Jorge Sainz Sueiro. All rights reserved.
+//
+
+import Foundation
